a3edb1a653eac692002474cbd576714d8dcc4605
Gian <gianmmarelli@gmail.com>
2023-04-15 00:02:48 -0300

    signal working, code needs refactoring

--- app/models/checks_model.py
+++ app/models/checks_model.py
@@ -13,10 +13,13 @@ class QuotationStatus(models.TextChoices):
 
 class CheckModel(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # client = models.ForeignKey(ClientModel, on_delete=models.CASCADE, db_constraint=False, related_name="checks")
     client_check_key = models.ForeignKey(ClientModel, on_delete=models.DO_NOTHING, related_name="client_check", blank=True, null=True)
     check_amount = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     quotation_status = models.TextField(choices=QuotationStatus.choices, default=QuotationStatus.PENDING.value)
     amount_client_recieves = models.FloatField(null=True, blank=True)
     expiration_date = models.DateField()
+    # emisor/librador del cheque string
+    # n cuenta emisor del cheque
+    # n cheque querie filtrar por n cheque
+    # a futuro agregar foto del cheque
